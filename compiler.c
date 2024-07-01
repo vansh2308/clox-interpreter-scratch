1d0c7af0358de7594c23dc77cb1395bae9b92be2
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-07-01 18:24:52 +0530

    Add if-else operations

--- compiler.c
+++ compiler.c
@@ -133,6 +133,13 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
   emitByte(byte2);
 }
 
+static int emitJump(uint8_t instruction) {
+  emitByte(instruction);
+  emitByte(0xff);
+  emitByte(0xff);
+  return currentChunk()->count - 2;
+}
+
 static void emitReturn() {
   emitByte(OP_RETURN);
 }
@@ -150,6 +157,20 @@ static uint8_t makeConstant(Value value) {
 static void emitConstant(Value value) {
   emitBytes(OP_CONSTANT, makeConstant(value));
 }
+
+
+static void patchJump(int offset) {
+  // -2 to adjust for the bytecode for the jump offset itself.
+  int jump = currentChunk()->count - offset - 2;
+
+  if (jump > UINT16_MAX) {
+    error("Too much code to jump over.");
+  }
+
+  currentChunk()->code[offset] = (jump >> 8) & 0xff;
+  currentChunk()->code[offset + 1] = jump & 0xff;
+}
+
 
 static void initCompiler(Compiler* compiler) {
   compiler->localCount = 0;
@@ -204,6 +225,9 @@ static int resolveLocal(Compiler* compiler, Token* name) {
   for (int i = compiler->localCount - 1; i >= 0; i--) {
     Local* local = &compiler->locals[i];
     if (identifiersEqual(name, &local->name)) {
+      if (local->depth == -1) {
+        error("Can't read local variable in its own initializer.");
+      }
       return i;
     }
   }
@@ -248,6 +272,12 @@ static uint8_t parseVariable(const char* errorMessage) {
   if (current->scopeDepth > 0) return 0;
 
   return identifierConstant(&parser.previous);
+}
+
+
+static void markInitialized() {
+  current->locals[current->localCount - 1].depth =
+      current->scopeDepth;
 }
 
 static void defineVariable(uint8_t global) {
@@ -320,6 +350,24 @@ static void expressionStatement() {
   emitByte(OP_POP);
 }
 
+static void ifStatement() {
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition."); 
+
+  int thenJump = emitJump(OP_JUMP_IF_FALSE);
+  emitByte(OP_POP);
+  statement();
+
+  int elseJump = emitJump(OP_JUMP);
+
+  patchJump(thenJump);
+  emitByte(OP_POP);
+
+  if (match(TOKEN_ELSE)) statement();
+  patchJump(elseJump);
+}
+
 static void printStatement() {
   expression();
   consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -363,7 +411,9 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
-  } else if (match(TOKEN_LEFT_BRACE)) {
+  } else if (match(TOKEN_IF)) {
+    ifStatement();
+  }  else if (match(TOKEN_LEFT_BRACE)) {
     beginScope();
     block();
     endScope();
